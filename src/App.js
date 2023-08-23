43c7b05bd25a2e87330102e38f653719805d44ce
Yosra Nabil <yoyitanabil@gmail.com>
2023-08-23 14:47:25 +0300

    Uploaded Files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div id="nav">
+  <ul>
+     <li><a href="index.html">Home</a></li>
+     <li><a href="about.html">About</a></li>
+     <li><a href="products.html">Products</a></li>
+     <li><a href="support.html">Support</a></li>
+     <li><a href="contact.html">Contact us</a></li>
+  </ul>
+</div>
+  );
+}
+
+export default App;
